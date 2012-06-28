f78c6dfe43e87d85666123956be9c59070ca4339
Roberto Olivares-Amaya <robolivares@gmail.com>
2012-06-28 17:29:46 -0400

    added one comment

--- density.C
+++ density.C
@@ -205,6 +205,7 @@ public:
 };
 
 // accumulates into dm
+// This is the noise that you set out in the schedule
 void DensityMatrix::add_onedot_noise(const std::vector<Wavefunction>& wave_solutions, SpinBlock& big, const double noise, bool act2siteops)
 {
 /* check normalisation */
@@ -220,6 +221,7 @@ void DensityMatrix::add_onedot_noise(const std::vector<Wavefunction>& wave_solut
   SpinBlock* leftBlock = big.get_leftBlock();
   if (dmrginp.outputlevel() != 0) 
     pout << "\t\t\t Modifying density matrix " << endl;
+    pout << "\t\t\t noise before modification " << noise << endl;
   //int maxt = 1;
   DensityMatrix* dmnoise = new DensityMatrix[MAX_THRD];
   for(int j=0;j<MAX_THRD;++j)
