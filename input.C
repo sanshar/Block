29cc10a571eb2cc09222d9fe08dff2b32dbd7984
robolivares <robolivares@gmail.com>
2013-09-04 17:07:04 -0400

    added startm key and streamlined default schedule

--- input.C
+++ input.C
@@ -122,6 +122,7 @@ void SpinAdapted::Input::initialize_defaults()
   m_num_spatial_orbs = 0;
   m_schedule_type_default = false;
   m_maxM = 0;
+  m_startM = 500;
   m_core_energy = 0.0;
   m_reorder = false;
   m_reorderfile = "";
@@ -190,6 +191,26 @@ SpinAdapted::Input::Input(const string& config_name)
 
 	if (m_maxM <= 0) {
 	  pout << "maxM cannot be less than equal to 0"<<endl;
+	  pout << "error found in the following line "<<endl;
+	  pout << msg<<endl;
+	  abort();
+	}	
+	  
+      }
+      else if (boost::iequals(keyword, "startM")) {
+	if(usedkey[STARTM] == 0) 
+	  usedkey_error(keyword, msg);
+	usedkey[STARTM] = 0;
+	if (tok.size() != 2) {
+	  pout << "keyword startM should be followed by a single number and then an end line"<<endl;
+	  pout << "error found in the following line "<<endl;
+	  pout << msg<<endl;
+	  abort();
+	}	
+	m_startM = atoi(tok[1].c_str());
+
+	if (m_startM < 50) {
+	  pout << "maxM cannot be less than 50"<<endl;
 	  pout << "error found in the following line "<<endl;
 	  pout << msg<<endl;
 	  abort();
@@ -1171,6 +1192,12 @@ void SpinAdapted::Input::performSanityTest()
       pout << "maxM cannot be less than 0"<<endl;
       abort();
     }
+    if (m_maxM < m_startM) {
+       pout << "maxM is smaller than startM" << endl;
+       pout << "Make sure you specify a maxM larger than " << m_startM << endl;
+       pout << "or specify a smaller startM " << endl;
+       abort();
+    }
 
     if (m_sweep_tol <= 0.0) {
       pout << "Using the default tolerance sweep tolerance of 1.0e-5."<<endl;
@@ -1186,194 +1213,57 @@ void SpinAdapted::Input::performSanityTest()
     
     double sweeptol = m_sweep_tol;
     int lastiter = 0;
-    if (m_norbs <24) {
-       if (m_maxM >= 50) {
-         m_sweep_iter_schedule.push_back(0); m_sweep_state_schedule.push_back(50); m_sweep_tol_schedule.push_back(1.0e-5);  m_sweep_noise_schedule.push_back(1.0e-4);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(0); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(1.0e-6);  m_sweep_noise_schedule.push_back(1.0e-4);
-       }
-       if (m_maxM >50) {
-       if (m_maxM >= 100) {
-         m_sweep_iter_schedule.push_back(4); m_sweep_state_schedule.push_back(100); m_sweep_tol_schedule.push_back(1.0e-5);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(4); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(1.0e-5);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
-       if (m_maxM >100) {
-       if (m_maxM >= 250) {
-         m_sweep_iter_schedule.push_back(8); m_sweep_state_schedule.push_back(250); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(8); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
-       if (m_maxM >250) {
-       if (m_maxM >= 500) {
-         m_sweep_iter_schedule.push_back(12); m_sweep_state_schedule.push_back(500); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(12); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
-       if (m_maxM >500) {
-       if (m_maxM >= 1000) {
-         m_sweep_iter_schedule.push_back(16); m_sweep_state_schedule.push_back(1000); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(16); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
-       if (m_maxM >1000) {
-       if (m_maxM >= 2000) {
-         m_sweep_iter_schedule.push_back(19); m_sweep_state_schedule.push_back(2000); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(19); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
+    int firstSched = 0;
+    int sweepCount = 0;
+    int nSched=14;
+    int defM [] = {50, 100, 250, 500, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000};
+    int defIter [] = {8, 8, 8, 8, 8, 4, 4, 4, 4, 4, 4, 4, 4, 4};
+    double defNoise [] = {1.0e-4, 1.0e-4, 1e-4, 1e-4, 5e-5, 5e-5, 5e-5, 5e-5, 5e-5, 5e-5, 5e-5, 5e-5, 5e-5, 5e-5};
+    double defTol [] = {1.0e-5, 1.0e-5, 1e-5, 1e-5, 5e-5, 5e-6, 5e-6, 5e-6, 5e-6, 5e-6, 5e-6, 5e-6, 5e-6, 5e-6};
 
-       if (m_maxM >2000) {
-       if (m_maxM >= 4000) {
-         m_sweep_iter_schedule.push_back(22); m_sweep_state_schedule.push_back(4000); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(22); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
-       if (m_maxM >4000) {
-       if (m_maxM >= 6000) {
-         m_sweep_iter_schedule.push_back(25); m_sweep_state_schedule.push_back(6000); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(25); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
+    for (int i=0; i<nSched;++i){
+       if (firstSched==0){
+          if (m_startM == m_maxM){
+             //cout << sweepCount << " " << m_startM << " " << defTol[i] << " " << defNoise[i] << endl;
+             m_sweep_iter_schedule.push_back(sweepCount); m_sweep_state_schedule.push_back(m_startM); m_sweep_tol_schedule.push_back(5E-5);  m_sweep_noise_schedule.push_back(5E-6);
+             sweepCount += 8;
+             break;
+          }
 
-       if (m_maxM >6000) {
-       if (m_maxM >= 8000) {
-         m_sweep_iter_schedule.push_back(28); m_sweep_state_schedule.push_back(8000); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(28); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
+          else if ((m_startM == defM[i])){
+             //cout << sweepCount << " " << defM[i] << " " << defTol[i] << " " << defNoise[i] << endl;
+             firstSched = 1;
+             m_sweep_iter_schedule.push_back(sweepCount); m_sweep_state_schedule.push_back(defM[i]); m_sweep_tol_schedule.push_back(defTol[i]);  m_sweep_noise_schedule.push_back(defNoise[i]);
+             sweepCount += defIter[i];
+          }
+          else if ((m_startM < defM[i])){
+             firstSched = 1;
+             //cout << sweepCount << " " << m_startM << " " << defTol[i-1] << " " << defNoise[i-1] << endl;
+             m_sweep_iter_schedule.push_back(sweepCount); m_sweep_state_schedule.push_back(m_startM); m_sweep_tol_schedule.push_back(defTol[i-1]);  m_sweep_noise_schedule.push_back(defNoise[i-1]);
+             sweepCount += defIter[i-1];
 
-       if (m_maxM >8000) {
-       if (m_maxM >= 10000) {
-         m_sweep_iter_schedule.push_back(31); m_sweep_state_schedule.push_back(10000); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
+             cout << sweepCount << " " << defM[i] << " " << defTol[i] << " " << defNoise[i] << endl;
+             m_sweep_iter_schedule.push_back(sweepCount); m_sweep_state_schedule.push_back(defM[i]); m_sweep_tol_schedule.push_back(defTol[i]);  m_sweep_noise_schedule.push_back(defNoise[i]);
+             sweepCount += defIter[i];
+          }
        }
-       else {
-         m_sweep_iter_schedule.push_back(31); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
+       else{//After first iteration
+          if (defM[i]>=m_maxM){
+             //cout << sweepCount << " " << m_maxM << " " << defTol[i] << " " << defNoise[i] << endl;
+             m_sweep_iter_schedule.push_back(sweepCount); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(defTol[i]);  m_sweep_noise_schedule.push_back(defNoise[i]);
+             sweepCount += defIter[i];
+             break;
+          }
+          else{
+             //cout << sweepCount << " " << defM[i] << " " << defTol[i] << " " << defNoise[i] << endl;
+             m_sweep_iter_schedule.push_back(sweepCount); m_sweep_state_schedule.push_back(defM[i]); m_sweep_tol_schedule.push_back(defTol[i]);  m_sweep_noise_schedule.push_back(defNoise[i]);
+             sweepCount += defIter[i];
+          }
        }
-       }    
-       //In the case that we want to do only the minimum calculation
-       if (m_maxM==50){
-         m_sweep_iter_schedule.push_back(4); m_sweep_state_schedule.push_back(50); m_sweep_tol_schedule.push_back(1.0e-5);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       lastiter = m_sweep_iter_schedule.back();
-       m_sweep_iter_schedule.push_back(lastiter+2); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(sweeptol/10.0);  m_sweep_noise_schedule.push_back(0.0e-5);
     }
-    // Greater than 12 orbitals
-    else {
-       if (m_maxM < 500) {
-         pout << "The default schedule for an orbital space larger than 16 starts at M=500" << endl;
-         m_maxM=500;
-       }
-       if (m_maxM >= 500) {
-         m_sweep_iter_schedule.push_back(0); m_sweep_state_schedule.push_back(500); m_sweep_tol_schedule.push_back(1.0e-5);  m_sweep_noise_schedule.push_back(1.0e-4);
-       }
-       if (m_maxM >500) {
-       if (m_maxM >= 1000) {
-         m_sweep_iter_schedule.push_back(8); m_sweep_state_schedule.push_back(1000); m_sweep_tol_schedule.push_back(5.0e-5);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(8); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-5);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
-       if (m_maxM >1000) {
-       if (m_maxM >= 2000) {
-         m_sweep_iter_schedule.push_back(16); m_sweep_state_schedule.push_back(2000); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(16); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
-
-       if (m_maxM >2000) {
-       if (m_maxM >= 3000) {
-         m_sweep_iter_schedule.push_back(20); m_sweep_state_schedule.push_back(3000); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(20); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
-       if (m_maxM >3000) {
-       if (m_maxM >= 4000) {
-         m_sweep_iter_schedule.push_back(24); m_sweep_state_schedule.push_back(4000); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(24); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
-
-       if (m_maxM >4000) {
-       if (m_maxM >= 5000) {
-         m_sweep_iter_schedule.push_back(28); m_sweep_state_schedule.push_back(5000); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(28); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
-       if (m_maxM >5000) {
-       if (m_maxM >= 6000) {
-         m_sweep_iter_schedule.push_back(32); m_sweep_state_schedule.push_back(6000); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(32); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
-       if (m_maxM >6000) {
-       if (m_maxM >= 7000) {
-         m_sweep_iter_schedule.push_back(36); m_sweep_state_schedule.push_back(7000); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(36); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
-       if (m_maxM >7000) {
-       if (m_maxM >= 8000) {
-         m_sweep_iter_schedule.push_back(40); m_sweep_state_schedule.push_back(8000); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(40); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
-       if (m_maxM >8000) {
-       if (m_maxM >= 9000) {
-         m_sweep_iter_schedule.push_back(44); m_sweep_state_schedule.push_back(9000); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(44); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
-       if (m_maxM >9000) {
-       if (m_maxM >= 10000) {
-         m_sweep_iter_schedule.push_back(48); m_sweep_state_schedule.push_back(10000); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       else {
-         m_sweep_iter_schedule.push_back(48); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(5.0e-6);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
-       }
-
-       //In the case that we want to do only the minimum calculation
-       if (m_maxM==500){
-         m_sweep_iter_schedule.push_back(8); m_sweep_state_schedule.push_back(500); m_sweep_tol_schedule.push_back(5.0e-5);  m_sweep_noise_schedule.push_back(5.0e-5);
-       }
        lastiter = m_sweep_iter_schedule.back();
+       //lastiter = sweepCount;
        m_sweep_iter_schedule.push_back(lastiter+2); m_sweep_state_schedule.push_back(m_maxM); m_sweep_tol_schedule.push_back(sweeptol/10.0);  m_sweep_noise_schedule.push_back(0.0e-5);
-    }
 
 
     if (m_twodot_to_onedot_iter < 18 && m_algorithm_type == TWODOT_TO_ONEDOT) {
@@ -1382,8 +1272,8 @@ void SpinAdapted::Input::performSanityTest()
       m_twodot_to_onedot_iter = lastiter+4;
     }
     if (m_maxiter <= m_sweep_iter_schedule.back()) {
-      pout << "With the default schedule and maxM specified, maxiter has to be at least "<<lastiter+6<<endl;
-      pout << "changing maxiter to "<<lastiter+6<<endl;
+      //pout << "With the default schedule and maxM specified, maxiter has to be at least "<<lastiter+6<<endl;
+      //pout << "changing maxiter to "<<lastiter+6<<endl;
       m_maxiter = lastiter+6;
     }
   }
